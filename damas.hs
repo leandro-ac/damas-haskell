91b1d58938196bf8a34b1ca46ed94cc0387a66ca
leandro-ac <leandro.alvaresc@gmail.com>
2025-06-24 00:41:09 -0300

    Corrigindo alguns erros

--- damas.hs
+++ damas.hs
@@ -1,28 +1,25 @@
 -- Jogo de Damas em Haskell
 -- Autores:
--- Leandro ALvares de Carvalho 202065211A
--- Vinicius ....
+-- Leandro Alvares
+-- Vinicius
 
 import Data.Char (toUpper, chr, ord)
 import Data.Maybe (isJust)
 import System.IO (hFlush, stdout)
 import Control.Concurrent (threadDelay)
 
-module Main where
-
--- Tipos que representam os jogadores, peças, casas e tabuleiro
+-- Tipos principais
 data Jogador = A | B deriving (Eq, Show)
 data Tipo = Piao | Dama deriving (Eq, Show)
 data Peca = Peca Jogador Tipo deriving (Eq, Show)
 data Casa = Clara | Escura (Maybe Peca) deriving (Eq, Show)
 
 type Tabuleiro = [[Casa]]
-type Pos = (Int, Int)  -- Linha, Coluna
+type Pos = (Int, Int)
 
--- Cria o tabuleiro inicial com peças do Jogador A nas 3 primeiras linhas
--- e do Jogador B nas 3 últimas, apenas nas casas escuras (linha + coluna par)
+-- Inicializa o tabuleiro
 tabuleiroInicial :: Tabuleiro
-tabuleiroInicial = [ [inicial l c | c <- [0..7]] | l <- [0..7] ]
+tabuleiroInicial = [ [inicial r c | c <- [0..7]] | r <- [0..7] ]
   where
     inicial l c
       | (l + c) `mod` 2 == 0 = Escura (pecaInicial l)
@@ -32,7 +29,7 @@ tabuleiroInicial = [ [inicial l c | c <- [0..7]] | l <- [0..7] ]
       | l >= 5 = Just (Peca B Piao)
       | otherwise = Nothing
 
--- Converte uma casa do tabuleiro em um símbolo visível no terminal
+-- Exibição
 mostrarCasa :: Casa -> String
 mostrarCasa Clara = " . "
 mostrarCasa (Escura Nothing) = "   "
@@ -41,19 +38,13 @@ mostrarCasa (Escura (Just (Peca A Dama))) = " A "
 mostrarCasa (Escura (Just (Peca B Piao))) = " b "
 mostrarCasa (Escura (Just (Peca B Dama))) = " B "
 
--- Imprime o tabuleiro com letras (A–H) e números (1–8)
 mostrarTabuleiro :: Tabuleiro -> String
 mostrarTabuleiro tab =
   let cabecalho = "   " ++ concat [ " " ++ [chr (c + 65)] ++ " " | c <- [0..7] ]
       linhas = [ show (8 - r) ++ " " ++ concatMap mostrarCasa linha | (r, linha) <- zip [0..] (reverse tab) ]
   in unlines (cabecalho : linhas)
 
--- Direções válidas para movimentar um peão (frente-esquerda e frente-direita)
-direcoesPiao :: Jogador -> [(Int, Int)]
-direcoesPiao A = [(1, -1), (1, 1)]
-direcoesPiao B = [(-1, -1), (-1, 1)]
-
--- Retorna a peça presente na posição informada, se houver
+-- Auxiliares
 emJogo :: Tabuleiro -> Pos -> Maybe Peca
 emJogo tab (l,c)
   | l < 0 || l > 7 || c < 0 || c > 7 = Nothing
@@ -61,7 +52,6 @@ emJogo tab (l,c)
                   Escura mp -> mp
                   _ -> Nothing
 
--- Verifica se a posição está dentro do tabuleiro
 dentro :: Pos -> Bool
 dentro (l,c) = l >= 0 && l <= 7 && c >= 0 && c <= 7
 
