add5237408f87fb237992016e919ea993f1a2e49
leandro-ac <leandro.alvaresc@gmail.com>
2025-06-23 22:10:50 -0300

    Implementa tabuleiro inicial

--- damas.hs
+++ damas.hs
@@ -13,3 +13,16 @@ data Casa = Clara | Escura (Maybe Peca) deriving (Eq, Show)
 
 type Tabuleiro = [[Casa]]
 type Pos = (Int, Int)  -- Linha, Coluna
+
+-- Cria o tabuleiro inicial com peças do Jogador A nas 3 primeiras linhas
+-- e do Jogador B nas 3 últimas, apenas nas casas escuras (linha + coluna par)
+tabuleiroInicial :: Tabuleiro
+tabuleiroInicial = [ [inicial l c | c <- [0..7]] | l <- [0..7] ]
+  where
+    inicial l c
+      | (l + c) `mod` 2 == 0 = Escura (pecaInicial l)
+      | otherwise            = Clara
+    pecaInicial l
+      | l <= 2 = Just (Peca A Piao)
+      | l >= 5 = Just (Peca B Piao)
+      | otherwise = Nothing
