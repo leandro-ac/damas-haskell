c2d0ae5d3bef63557d10ad28a2dd6739be9d6cdb
leandro-ac <leandro.alvaresc@gmail.com>
2025-06-24 00:07:08 -0300

    Implementa movimentos válidos para peças

--- damas.hs
+++ damas.hs
@@ -44,3 +44,31 @@ mostrarTabuleiro tab =
   let cabecalho = "   " ++ concat [ " " ++ [chr (c + 65)] ++ " " | c <- [0..7] ]
       linhas = [ show (8 - r) ++ " " ++ concatMap mostrarCasa linha | (r, linha) <- zip [0..] (reverse tab) ]
   in unlines (cabecalho : linhas)
+
+-- Direções válidas para movimentar um peão (frente-esquerda e frente-direita)
+direcoesPiao :: Jogador -> [(Int, Int)]
+direcoesPiao A = [(1, -1), (1, 1)]
+direcoesPiao B = [(-1, -1), (-1, 1)]
+
+-- Retorna a peça presente na posição informada, se houver
+emJogo :: Tabuleiro -> Pos -> Maybe Peca
+emJogo tab (l,c)
+  | l < 0 || l > 7 || c < 0 || c > 7 = Nothing
+  | otherwise = case tab !! l !! c of
+                  Escura mp -> mp
+                  _ -> Nothing
+
+-- Verifica se a posição está dentro do tabuleiro
+dentro :: Pos -> Bool
+dentro (l,c) = l >= 0 && l <= 7 && c >= 0 && c <= 7
+
+-- Retorna os movimentos possíveis para uma peça simples
+movimentosPeca :: Tabuleiro -> Jogador -> Pos -> [Pos]
+movimentosPeca tab j (l,c) =
+  case emJogo tab (l,c) of
+    Just (Peca _ Piao) ->
+      [ (l+dl, c+dc) |
+        (dl, dc) <- direcoesPiao j,
+        dentro (l+dl, c+dc),
+        emJogo tab (l+dl, c+dc) == Nothing ]
+    _ -> []
